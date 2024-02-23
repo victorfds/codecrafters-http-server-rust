cdc77c12a7438fce369b1a10dd4509ad6289b7a0
Victor Ferreira da Silva Baruel <victorxpro@yahoo.com>
2024-02-22 22:24:43 -0400

    test

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,7 @@ fn main() {
 fn handle_connection(stream: &mut TcpStream) {
     let response = "HTTP/1.1 200 OK\r\n\r\n";
 
+    print!("TEST");
     stream
         .write_all(response.as_bytes())
         .expect("Failed to stream write.");
