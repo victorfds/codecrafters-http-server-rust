82f65cfed3a8794ec2647a001504c4c13c47c7e0
Victor Ferreira da Silva Baruel <victorxpro@yahoo.com>
2024-02-22 22:20:07 -0400

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -7,12 +10,20 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                handle_connection(&mut stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(stream: &mut TcpStream) {
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream
+        .write_all(response.as_bytes())
+        .expect("Failed to stream write.");
 }
