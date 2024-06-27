b435d295f27f34ffe6a04c5892e3e656fb6d55f2
Victor Ferreira da Silva Baruel <victorxpro@yahoo.com>
2024-06-26 23:09:37 -0400

    Impl echo fn

--- src/main.rs
+++ src/main.rs
@@ -21,17 +21,30 @@ fn main() {
 }
 
 fn handle_connection(stream: &mut TcpStream) {
-    let mut buffer = [0; 512];
+    let mut buffer = [0; 1024];
 
-     stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let request = String::from_utf8_lossy(&buffer[..]);
+    let lines: Vec<&str> = request.lines().collect();
+    let first_line = *lines.first().unwrap();
+    let first_line_only_words: Vec<&str> = first_line.split_whitespace().collect();
 
-    let response = if buffer.starts_with(b"GET / HTTP/1.1\r\n") {
-        "HTTP/1.1 200 OK\r\n\r\n"
+    println!("Request: {}", request);
+
+    let response = if first_line.starts_with("GET / HTTP/1.1") {
+        "HTTP/1.1 200 OK\r\n\r\n".to_string()
+    } else if first_line.starts_with("GET /echo/") {
+        let to_echo = first_line_only_words.get(1).unwrap().split_at(6).1;
+        format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+            to_echo.len(),
+            to_echo,
+        )
     } else {
-        "HTTP/1.1 404 Not Found\r\n\r\n"
+        "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
     };
 
     stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
